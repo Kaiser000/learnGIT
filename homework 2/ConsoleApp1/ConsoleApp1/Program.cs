ea2b4931d5558157c886fbed0c98259665e7d5ed
Kaiser000 <Kaiser_ztf@163.com>
2020-03-02 12:00:00 +0800

    this is the second homework

---
+++ homework 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            try
+            {
+               
+                Console.WriteLine("please enter an integer number");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n == 1)
+                {
+                    Console.WriteLine("this number hasn't prime factor");
+                    return;
+                }              
+                for (int i = 2; i*i <= n; i++)    //算法优化
+                {
+                    while (n % i == 0)
+                    {
+                        n = n / i;
+                        Console.Write(i + "     ");
+                    }                
+                }
+                Console.Write(n + "     ");      //最后一个除不尽的也是质数因子
+            }
+            catch(Exception e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+    }
+}
---
+++ homework 2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void GetMax(int[] a)
+        {
+            int max= -2147483648;
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+            Console.WriteLine("" + max);
+            return;
+        }
+        static void GetMin(int[] a)
+        {
+            int min = 2147483647;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] < min)
+                {
+                    min = a[i];
+                }
+            }
+            Console.WriteLine("" + min);
+            return;
+        }
+        static void GetAvg(int[] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            Console.WriteLine("" + sum/a.Length);
+            return;
+
+        }
+        static void GetSum(int[] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            Console.WriteLine("" + sum);
+            return;
+
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 343, 4, 4, 9 }; 
+            try
+            {
+                GetMax(a);
+                GetMin(a);
+                GetAvg(a);
+                GetSum(a);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+        }
+    }
+}
