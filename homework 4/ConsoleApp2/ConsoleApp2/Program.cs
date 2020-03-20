509d8396d32e8cd1ee2d6631cace4128897c95f0
Kaiser000 <Kaiser_ztf@163.com>
2020-03-20 08:09:07 +0800

    4th

---
+++ homework 4/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public class Node<T>  //链表节点
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+            public GenericList()
+            {
+                tail = head = null;
+            }
+            public Node<T> Head
+            {
+                get;
+            }
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                    tail = n;
+                }
+            }
+           public void ForEach(Action<T> action)
+            {
+                for(Node<T> i = Head; i != null; i = i.Next)
+                {
+                    action(i.Data);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            int sum=0;
+            int max=-99999999;
+            int min=99999999;
+            Action<int> Action1 = m => Console.WriteLine(m);
+            Action<int> Action2 = m => sum += m;
+            Action<int> Action3 = m => { if (m > max) max = m; };
+            Action<int> Action4 = m => { if (m < min) min = m; };
+            list.ForEach(Action1);
+            list.ForEach(Action2);
+            list.ForEach(Action3);
+            list.ForEach(Action4);
+
+
+
+        }
+    }
+}
---
+++ homework 4/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class ClockEventArgs {}
+    public delegate void ClockHandler(object sender, ClockEventArgs args);
+    
+    public class Clock
+    {       
+        public event ClockHandler StartTick;
+        public event ClockHandler StartAlarm;
+        public bool Pass(ref DateTime Time)
+        {
+                Console.WriteLine("当前时间：" + Time.ToString("HH-mm-ss"));
+                System.Threading.Thread.Sleep(1000);
+                 Time= Time.AddSeconds(1);
+            return true;
+        }
+       
+        public void Tick(ref DateTime Time)
+        {            
+            Pass(ref Time);        
+            Console.WriteLine("Tick");
+          
+           // StartTick(this,new ClockEventArgs());
+        }
+        public void Alarm(DateTime T,ref DateTime Time)
+        {
+
+            TimeSpan timeSpan = T - Time;        
+            if (timeSpan.Seconds<=1&&timeSpan.Seconds>0)//因为这样计时不准，必须用时间差进行统计
+            {
+                Console.WriteLine("Alarm!");
+            }
+            //StartAlarm(this,new ClockEventArgs());
+        }
+    }
+    public class Form
+    {
+        public Clock clock = new Clock();
+        public Form()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime T = DateTime.Now.AddSeconds(5);
+            Console.WriteLine(T.ToString());
+            DateTime Time = DateTime.Now;
+            while (true)
+            {
+               
+                Form form = new Form();
+                form.clock.Tick(ref Time);
+                form.clock.Alarm(T,ref Time);
+            }
+          
+           
+        }
+    }
+}
