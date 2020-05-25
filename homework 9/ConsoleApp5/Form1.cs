c719c07363f39b15471f78c9da2bfd1a9b4a7fbc
Kaiser000 <Kaiser_ztf@163.com>
2020-05-26 00:18:49 +0800

    this is the 9th homework

---
+++ homework 9/ConsoleApp5/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp5
+{
+    public partial class Form1 : Form
+    {
+        Crawler mycrawler = new Crawler();
+        string startUrl;
+        string preurl;
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startUrl = textBox1.Text;
+            preurl = startUrl.Remove(startUrl.IndexOf('/', 8) + 1);//获取url的根目录地址
+            mycrawler.urls.Add(startUrl, false);
+            mycrawler.Crawl(preurl);
+            foreach (string url in mycrawler.urls.Keys)
+            {
+                listBox1.Items.Add(url);
+            }
+        }
+    }
+}
