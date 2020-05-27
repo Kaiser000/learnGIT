fc9af50f10890e06ad1488a7b8b4b7ddd3c903d4
Kaiser000 <Kaiser_ztf@163.com>
2020-05-27 19:05:17 +0800

    11th

---
+++ homework 11/ConsoleApp10/OrderContext.cs
@@ -0,0 +1,23 @@
+namespace ConsoleApp10
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class OrderContext : DbContext
+    {
+        public OrderContext()
+             : base("OrderDataBase")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<OrderContext>());
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> Items { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
